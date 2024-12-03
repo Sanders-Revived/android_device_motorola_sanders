a57265ff622e9ff594360b2af79e2b6270033a55
L1ghtzin <lightzin021@gmail.com>
2024-12-03 09:53:41 -0300

    sanders: Remove FDE encryption for now * we will migrate to FBE soon

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,10 +107,6 @@ TARGET_USES_QTI_CAMERA_DEVICE := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 BOARD_NO_CHARGER_LED := true
 
-# Crypto
-TARGET_HW_DISK_ENCRYPTION := true
-TARGET_LEGACY_HW_DISK_ENCRYPTION := true
-
 # Display
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 TARGET_SCREEN_DENSITY := 480
--- device.mk
+++ device.mk
@@ -121,10 +121,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
