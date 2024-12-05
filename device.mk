111288ea944fef25320f8323afa8221b87a17b18
L1ghtzin <lightzin021@gmail.com>
2024-12-05 10:39:50 -0300

    sanders: Update thermal blobs from LA.UM.10.6.2.r1-02500-89xx.0

--- device.mk
+++ device.mk
@@ -388,9 +388,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8953
+    android.hardware.thermal@1.0-impl:64 \
+    android.hardware.thermal@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
